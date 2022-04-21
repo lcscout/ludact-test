b4f2714aad6f0911b595684016c0b702e91809ff
lcscout <lcscoutinho@outlook.com>
2022-04-21 17:10:13 -0300

    feat(GameManager): add GameModes and a restart method
    Now the game has two modes, the Confined one keep the ships confined in the camera area. The Free mode lets the ships pass the camera bounds but theyre warped to the opposite edge.
    
    Also add a restart method and a method to reset all variables on Awake, so the restart truly returns the game to its initial state and doesnt generate any bugs.

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,16 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+	public enum Mode {
+		Confined,
+		Free
+	}
+
 	public static event Action<bool> OnGamePausedOrResumed;
+	public static event Action OnGameModeChanged;
 
 	public static bool IsGamePaused { get; private set; }
 
+	public static Mode GameMode { get; private set; }
+
 	[Tooltip("The targeted fps limit")]
 	[SerializeField] private int _targetFrameRate = 60;
+	[SerializeField] private EdgeCollider2D _cameraBounds;
 
-	private void Awake() => LimitFrameRate();
+	private void Awake() {
+		ResetGameState();
+		LimitFrameRate();
+	}
 
 	public void PauseAndResume() {
 		if (IsGamePaused)
@@ -22,6 +35,30 @@ public class GameManager : MonoBehaviour {
 		IsGamePaused = !IsGamePaused;
 
 		OnGamePausedOrResumed?.Invoke(IsGamePaused);
+	}
+
+	public void ChangeGameMode() {
+		if (GameMode == Mode.Confined) {
+			GameMode = Mode.Free;
+			_cameraBounds.isTrigger = true;
+		}
+		else {
+			GameMode = Mode.Confined;
+			_cameraBounds.isTrigger = false;
+		}
+
+		OnGameModeChanged?.Invoke();
+	}
+
+	public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+	private void ResetGameState() {
+		IsGamePaused = false;
+		GameMode = Mode.Confined;
+		Time.timeScale = 1f;
+
+		if (_cameraBounds != null)
+			_cameraBounds.isTrigger = false;
 	}
 
 	private void LimitFrameRate() {
